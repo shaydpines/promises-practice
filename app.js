b778c1bbeadfd79f61d90bdbebd0d428523131a4
shaydpines <oceanrooster@gmail.com>
2025-07-09 11:10:29 -0400

    If/else seems to block try/catch

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-
-const statusRef = document.querySelector('.status')
-const videoText = document.querySelector('.video__text')
+const statusRef = document.querySelector(".status");
+const videoText = document.querySelector(".video__text");
 
 function getSubscriptionStatus() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('VIP');
+      resolve();
     }, 2000);
   });
 }
 
 function getVideo(subscriptionStatus) {
   return new Promise((resolve, reject) => {
-    if (subscriptionStatus === 'VIP') {
-      resolve("show video")
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
     }
-    else if (subscriptionStatus === 'FREE') {
-      resolve("show trailer")
-    }
-    else {
-      reject("no video")
-    }
-  })
+  });
 }
 
 async function main() {
-  const status = (await getSubscriptionStatus());
-  statusRef.innerHTML = status
-  const videoStatus = (await getVideo(status))
-  videoText.innerHTML = videoStatus
-  try{
-    console.log(await getVideo(status))
-  }
-  catch (e) {
-    videoText.innerHTML = e
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  if ((status) === "VIP" || "FREE") {
+    const videoStatus = await getVideo(status);
+    videoText.innerHTML = videoStatus;
+  } else {
+    try {
+      console.log(await getVideo(status));
+    } catch (e) {
+      videoText.innerHTML = e;
+    }
   }
 }
-
 
 main();
